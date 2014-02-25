c7eebd414e1bfd854a40d78a9430e0c6f7762315
enlinea-tengteng <hweeteng.tan@enlinea.asia>
2014-02-25 13:39:46 +0800

    date

--- index.php
+++ index.php
@@ -3,12 +3,7 @@
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the 
+ * template in the editor.
  */
-hello  3
-
-banner
-
-hello world
-
-dell
+25/2/2014
